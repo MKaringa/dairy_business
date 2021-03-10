8730cdfd72c715ecff099aec657b53bf9e80821d
MKaringa <mungaikaringa1@gmail.com>
2021-03-10 15:26:45 +0300

    Created the sheds as an array

---
+++ dairybusiness.js
@@ -0,0 +1,18 @@
+
+var shed_A ={
+    shed_no : "A",
+    total_amount_of_milk_per_day : 510,
+};
+var shed_B ={
+    shed_no : "B",
+    total_amount_of_milk_per_day : 308,
+}
+var shed_C ={
+    shed_no : "C",
+    total_amount_of_milk_per_day : 486,
+}
+var shed_D ={
+    shed_no : "D",
+    total_amount_of_milk_per_day : 572,
+}
+var sheds =[shed_A, shed_B, shed_C, shed_D]
