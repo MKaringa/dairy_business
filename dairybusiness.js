3b873956fd03e9aa296033241e64a1f8bf2cf463
MKaringa <mungaikaringa1@gmail.com>
2021-03-14 12:17:08 +0300

    Created total Function

--- dairybusiness.js
+++ dairybusiness.js
@@ -1,4 +1,4 @@
-
+/*
 var shed_A ={
     shed_no : "A",
     total_amount_of_milk_per_day : 510,
@@ -16,3 +16,23 @@ var shed_D ={
     total_amount_of_milk_per_day : 572,
 }
 var sheds =[shed_A, shed_B, shed_C, shed_D]
+var totalMilk = function(){
+        return shed_A.total_amount_of_milk_per_day + shed_B.total_amount_of_milk_per_day + shed_C.total_amount_of_milk_per_day + shed_D.total_amount_of_milk_per_day;
+}
+console.log(shed_A.total_amount_of_milk_per_day + shed_B.total_amount_of_milk_per_day + shed_C.total_amount_of_milk_per_day + shed_D.total_amount_of_milk_per_day)
+for(i=0; i<sheds; i++){
+    console(i)
+}
+*/
+
+function findTotal(){
+    var arr = document.getElementsByName('qty');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+    }
+    document.getElementById('total').value = tot;
+}
+  
+
